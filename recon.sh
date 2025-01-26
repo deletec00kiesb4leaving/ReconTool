01b6e9873e39756f026fd7e0dec9dfb2c64d5f8b
deletec00kiesb4leaving <165312656+deletec00kiesb4leaving@users.noreply.github.com>
2025-01-26 16:05:25 +0000

    ReconTool Initial Upload

---
+++ recon.sh
@@ -0,0 +1,149 @@
+#!/bin/bash
+
+# API KEYS
+API_URLSCAN= #ADD YOUR OWN KEY HERE
+API_VIRUSTOTAL= #ADD YOUR OWN KEY HERE
+
+
+
+
+
+
+
+
+## Code Starts here:
+
+# Clear the terminal screen before starting
+clear
+clear
+clear
+echo "__________                             ___________           .__
+\______   \ ____   ____  ____   ____   \__    ___/___   ____ |  |  
+ |       _// __ \_/ ___\/  _ \ /    \    |    | /  _ \ /  _ \|  |  
+ |    |   \  ___/\  \__(  <_> )   |  \   |    |(  <_> |  <_> )  |__
+ |____|_  /\___  >\___  >____/|___|  /   |____| \____/ \____/|____/
+        \/     \/     \/           \/                              
+
+"
+
+
+# Create the directory
+dir_name=$(date +"%b%d_%Y_%H%M%S")
+mkdir "$dir_name"
+
+
+# Function to check if the input is an IP address (IPv4 or IPv6)
+is_ip_address() {
+    local ip=$1
+    if [[ $ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]] || [[ $ip =~ ^([0-9a-fA-F]{1,4}:){1,7}[0-9a-fA-F]{1,4}$ ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+
+# Get the local host IP
+LOCALHOST_IP=$(curl -s "https://ifconfig.so")
+LOCALHOST_LOCATION=$(curl -s "https://ipinfo.io/$LOCALHOST_IP/json" -o "$dir_name/localhost_ipinfo.json" )
+LOCALHOST_CITY=$(curl -s "https://ipinfo.io/$LOCALHOST_IP/json" | jq '.city')
+LOCALHOST_REGION=$(curl -s "https://ipinfo.io/$LOCALHOST_IP/json" | jq '.region')
+LOCALHOST_COUNTRY=$(curl -s "https://ipinfo.io/$LOCALHOST_IP/json" | jq '.country')
+
+
+# Check if an argument is provided
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <domain_or_ip>"
+    exit 1
+fi
+
+
+echo -e "\n\n=================================================="
+echo "Initializing scan..."
+echo "Scanning from host ip: $LOCALHOST_IP" 
+echo "Approximate Location: $LOCALHOST_CITY, $LOCALHOST_REGION, $LOCALHOST_COUNTRY"
+
+# Ask the user if they want to continue
+read -p "Do you want to continue? (y/n): " answer
+
+# Check the user's answer
+if [ "$answer" == "n" ] || [ "$answer" == "N" ] || [ "$answer" == "NO" ] || [ "$answer" == "no" ]; then
+    echo "Exiting program."
+    echo -e "==================================================\n\n"
+    exit 0
+fi
+echo -e "==================================================\n\n"
+
+
+echo "=================================================="
+echo -e "Starting Scan:\n"
+
+# Assign the first argument to TARGET
+TARGET="$1"
+
+# Check if TARGET is an IP address
+if is_ip_address "$TARGET"; then
+    TARGET_IP="$TARGET"
+    echo "Target IP Address: $TARGET_IP"
+    VT_SCAN=$(source ./vt-scan/vt-scan.sh -k $API_VIRUSTOTAL -i $TARGET_IP)
+    echo "$VT_SCAN" > $dir_name/vt-scan.json
+    echo "Using VirusTotal:"
+    echo "New file created at: ./$dir_name/vt-scan.json"
+else
+    # If it's not an IP, use ping to resolve the domain to an IP address
+    PING_OUTPUT=$(ping -c 1 "$TARGET" 2>/dev/null)
+    
+    # Check for IPv4 address
+    TARGET_IP=$(echo "$PING_OUTPUT" | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | head -n 1)
+    
+    # Check if we got an IP address
+    if [ -z "$TARGET_IP" ]; then
+        echo "Failed to resolve IPv4 address for $TARGET"
+        exit 1
+    fi
+
+    SCAN_URLSCAN=$(curl -s -X POST "https://urlscan.io/api/v1/scan/" -H "Content-Type: application/json" -H "API-Key: $API_URLSCAN" -d "{ \"url\": \"$TARGET\", \"visibility\": \"public\" }" -o "$dir_name/urlscan.json")
+
+    SCAN_RESULT=$(echo "$SCAN_URLSCAN" | grep "result")
+    
+    echo "Target Domain: $TARGET"
+    echo "Target IPv4 Address: $TARGET_IP"
+    echo -e "\nUsing:  urlscan.io"
+    echo "New file created at: ./$dir_name/urlscan.json"
+
+    VT_SCAN=$(source ./vt-scan/vt-scan.sh -k $API_VIRUSTOTAL -u $TARGET)
+    echo "$VT_SCAN" > $dir_name/vt-scan.json
+    echo -e "\nUsing VirusTotal:"
+    echo "New file created at: ./$dir_name/vt-scan.json"
+fi
+
+echo -e "\nUsing: ipinfo.io"
+
+# Use ipinfo.io to get location data for the IP address
+TARGET_LOCATION=$(curl -s "https://ipinfo.io/$TARGET_IP/json" -o "$dir_name/ipinfo.json" )
+echo "New file created at: ./$dir_name/ipinfo.json"
+
+# Echo the location data
+echo "$TARGET_LOCATION"
+
+echo "Using who.is on $TARGET:"
+
+# Use whois to get whois information
+WHOIS_INFO=$(whois "$TARGET")
+echo "$WHOIS_INFO" > $dir_name/whois.md
+echo "New file created at: ./$dir_name/whois.md"
+
+echo -e "=================================================="
+
+curl -s -H "Content-Type: application/json" -H "API-Key: $API_URLSCAN" "https://urlscan.io/user/quotas/" -o "$dir_name/urlscan_quota.json"
+
+
+echo -e "\n\n=================================================="
+echo -e "Useful Links:\n"
+echo "https://www.virustotal.com/gui/domain/$TARGET" 
+echo "https://search.onyphe.io/search?q=domain:$TARGET"
+echo "https://search.odin.io/hosts?query=$TARGET"
+echo "https://www.infobyip.com/ip-$TARGET.html"
+echo "Further ip/domain forensics: https://hackertarget.com/"
+echo "OSINT Tools: https://osintframework.com/"
+echo -e "=================================================="
---
+++ vt-scan/README.md
@@ -0,0 +1,38 @@
+# vt-scan: VirusTotal Scan Script  
+Simple bash script that allows you to either submit a file object for scan by VirusTotal or retrieve a report from a VirusTotal scan.  
+
+
+Why bash? Because it's easily available on most Linux boxes and doesn't require installing a separate interpreter or additional overhead (libraries, smaller footprint).  
+
+
+For the record, I'm sure similar projects already exist, but since I quickly created this out of necessity, I figured that I'd share it anyway.  
+
+## Requirements  
+This is a bash script, so the requirements are:  
+    * bash  
+    * curl  
+    * jq  
+
+This script also requires that you supply your own API token for VT. You may sign up for VT (and then generate a token) here: https://www.virustotal.com/gui/join-us.  
+
+## Usage  
+VirusTotal Scan Script for API V3  
+Interact with VT from your shell.  
+  
+Required parameters: API token, Action to perform.  
+Usage example: `./vt-scan.sh -k <API TOKEN> -f <FILE PATH>`  
+
+	-k		API key for VirusTotal - REQUIRED.  
+	-f		FULL PATH to a file object for VT to scan.  
+	-u		Submit URL for VT scan.  
+	-d		Submit domain for VT scan.  
+	-i		Submit IP for VT scan.  
+	-a		Retrieve analysis for existing scan, expects base64 object ID.  
+	-v		Display version information.  
+	-h		Display this help text with usage information.  
+  
+## TODO:  
+* Allow reading API token from local file (e.g. .vtscan/token)  
+* Integrate additional public APIs from VT  
+* Integrate addition private APIs from VT  
+* Pretty-print output formatting; human-readable terminal output  
